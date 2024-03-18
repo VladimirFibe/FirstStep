4b865312a368e7f58855311cc9e4a1d9ef29db65
Vladimir Fibe <motiw@icloud.com>
2024-03-19 04:57:25 +0500

    Initial Commit

---
+++ FirstStep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstStep
+//
+//  Created by Vladimir Fibe on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
