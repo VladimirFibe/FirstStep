6306ce46243d7ab471616dc8b3809fb094499061
Vladimir Fibe <motiw@icloud.com>
2024-03-22 07:20:05 +0500

    Add animation

--- FirstStep/AuthViewController.swift
+++ FirstStep/AuthViewController.swift
@@ -1,35 +1,103 @@
 import UIKit
 
 class AuthViewController: BaseViewController {
+    private var isLogin = true { didSet { updateUI() }}
     private let emailTextField = AuthTextField(placeholder: "Email")
     private let passwordTextField = AuthTextField(placeholder: "Password", isSecureTextEntry: true)
+    private let repeatTextField = AuthTextField(placeholder: "Repeat Password", isSecureTextEntry: true)
+    private let forgotButton: UIButton = {
+        $0.contentHorizontalAlignment = .leading
+        $0.setTitle("Forgot Password?", for: [])
+        return $0
+    }(UIButton(type: .system))
+    private let resendButton: UIButton = {
+        $0.contentHorizontalAlignment = .trailing
+        $0.isHidden = true
+        $0.setTitle("Resend Email", for: [])
+        return $0
+    }(UIButton(type: .system))
+    private let middleView = UIView()
+
+    private let actionButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Login"
+        config.cornerStyle = .medium
+        $0.configuration = config
+        return $0
+    }(UIButton(type: .system))
     private let rootStackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
     }(UIStackView())
+    private let authStatusSwitch = AuthStatusSwitch()
 }
-
+// MARK: - Actions
+extension AuthViewController {
+    @objc private func authSwitchTapped() {
+        isLogin.toggle()
+    }
+}
+// MARK: - Setup Views
 extension AuthViewController {
     override func setupViews() {
         super.setupViews()
-        [rootStackView].forEach {
+        [rootStackView, actionButton, authStatusSwitch].forEach {
             view.addSubview($0)
         }
+        authStatusSwitch.configure(self, action: #selector(authSwitchTapped))
         setupRootStackView()
+        updateUI()
     }
 
     private func setupRootStackView() {
-        [emailTextField, passwordTextField].forEach {
+        [emailTextField, passwordTextField, repeatTextField, middleView].forEach {
             rootStackView.addArrangedSubview($0)
         }
+        middleView.addSubview(forgotButton)
+        middleView.addSubview(resendButton)
     }
+
+    private func updateUI() {
+        authStatusSwitch.configure(with: isLogin)
+        actionButton.configuration?.title = isLogin ? "Login" : "Register"
+        navigationItem.title = isLogin ? "Login" : "Register"
+        UIView.animate(withDuration: 0.5) {
+            self.repeatTextField.isHidden = self.isLogin
+            self.repeatTextField.alpha = self.isLogin ? 0 : 1
+            self.middleView.isHidden = !self.isLogin
+            self.middleView.alpha = self.isLogin ? 1 : 0
+        }
+    }
+
     override func setupConstraints() {
         let padding = 20.0
 
         rootStackView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
             $0.leading.trailing.equalToSuperview().inset(padding)
+        }
+
+        actionButton.snp.makeConstraints {
+            $0.top.equalTo(rootStackView.snp.bottom).offset(50)
+            $0.leading.trailing.equalToSuperview().inset(padding)
+            $0.height.equalTo(50)
+        }
+
+        authStatusSwitch.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(50)
+        }
+
+        forgotButton.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+        }
+
+        resendButton.snp.makeConstraints {
+            $0.top.bottom.equalTo(forgotButton)
+            $0.trailing.equalToSuperview()
+            $0.leading.equalTo(forgotButton.snp.trailing)
+            $0.width.equalTo(forgotButton.snp.width)
         }
     }
 }
